145f14a7aa89a200216a498b6d360a721ca451ed
krikir58-ui <krikir58@gmail.com>
2026-09-18 20:30:30 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
 const CLOUD_NAME = "vi4etpob";
 const UPLOAD_PRESET = "pixoro_posts";
 const SUPABASE_URL = "https://yecpltndhlzfjplzhcpm.supabase.co";
+sb_publishable_byUfZPczrxfkYKTmUNco5w_oqabYQJ_
